77a1f46f13a6f23eb5a37620e8990ccceb7a886f
Bryan Alexis <66497776+Bryan0208@users.noreply.github.com>
2020-09-07 03:12:22 -0300

    Update Code.ino
    versionamiento de código de puente H

--- Puente H/Code.ino
+++ Puente H/Code.ino
@@ -1,24 +1,24 @@
 void setup()
 {
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(13,OUTPUT);
+  pinMode(8, OUTPUT); // |
+  pinMode(9, OUTPUT); // |
+  pinMode(13,OUTPUT); //Se definen terminales, todos como salida
 }
 
-void CambiaLed(){
-  digitalWrite(13,!digitalRead(13));
-}
+void CambiaLed(){                      //se crea función cambia led
+  digitalWrite(13,!digitalRead(13));  //se lee el estado del pin 13 el cual es led de control
+}                                     //y luego invierte el estado con "!", el led se apaga
 
 void loop()
 {
-  digitalWrite(9, HIGH);
-  digitalWrite(8, LOW);
-  delay(1000); // Espera 1 segundo (1000 milisegundos)
-  digitalWrite(9, HIGH);
-  digitalWrite(8, HIGH);
-  delay(500); // Espera 1 segundo (1000 milisegundos)
-  digitalWrite(8, HIGH);
-  digitalWrite(9, LOW);
-  delay(1000); // Espera 1 segundo (1000 milisegundos)
-  CambiaLed();
+  digitalWrite(9, HIGH);  //se lee pin 9 en alta
+  digitalWrite(8, LOW);   
+  delay(1000);            //motor gira a la derecha durante 1 segundo
+  digitalWrite(9, HIGH);  //se lee pin 9 en alta
+  digitalWrite(8, HIGH);  
+  delay(500);             //se detiene el motor durante 1 segundo
+  digitalWrite(8, HIGH);  //se lee el pin 8 e alta
+  digitalWrite(9, LOW);   //se lee el pin 9 en baja
+  delay(1000);            //Espera 1 segundo
+  CambiaLed();            //se llama la funcion cambia led
 }
